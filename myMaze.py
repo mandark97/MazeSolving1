5d6adc055953ad28068272ed3a6d6e9cd54aded3
Matei Melinte <melinte.matei@gmail.com>
2018-04-30 20:22:41 +0300

    added 10 random moving agents

--- myMaze.py
+++ myMaze.py
@@ -1,14 +1,15 @@
 # Author: William Zhang
-# Description: This is a maze solving algorithm. This is an implementation of the Wall Follower Algorithm. 
+# Description: This is a maze solving algorithm. This is an implementation of the Wall Follower Algorithm.
 # It uses the "left hand" of the robot as a means to solve the maze.
 # Date: April 23, 2018
 # Copyright 2018
 
 import sys, pygame
 from pygame.locals import *
+import random
 
 size_x = 32
-size_y = 32 
+size_y = 32
 
 change_x = 32
 change_y = 32
@@ -18,7 +19,21 @@ Size = 32
 SPEED = 32
 timer = 375
 
+COLORS = {
+    'DAY9YELLOR': (255, 167, 26),
+    'PURPLE': (144, 124, 180),
+    'PINK': (255, 124, 180),
+    'BLUE': (50, 50, 255),
+    'CYAN': (55, 202, 180),
+    'GREEN': (55, 202, 18),
+    'BROWN': (113, 49, 18),
+    'DARKBLUE': (0, 36, 255),
+    'LIGHTPURPLE': (178, 174, 255),
+    'DARKPURPLE': (178, 53, 255),
+}
+
 DAY9YELLOW = (255, 167, 26)
+
 GOAL = (245, 163, 183)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -39,7 +54,7 @@ clock = pygame.time.Clock()
            # 0 ------------------>                      21
 maze = [    [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],  # 0      y
             [1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1],  # 1      |
-            [1,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1],  # 2    \ | /  
+            [1,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1],  # 2    \ | /
             [1,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,1,1,1],  # 3     \ /
             [1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1],  # 4      `
             [1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1],  # 5
@@ -62,22 +77,22 @@ maze = [    [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],  # 0      y
 
 class Robot(pygame.sprite.Sprite):
 
-    def __init__(self, x, y): 
+    def __init__(self, x, y, color=DAY9YELLOW):
         # Call the parent's constructor
         super().__init__()
 
         # Set height, width
         self.image = pygame.Surface([Size, Size])
-        self.image.fill(DAY9YELLOW)
- 
+        self.image.fill(color)
+
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
-        
+
         self.face = 5
         self.lhs = 4
- 
+
         # Set speed vector
         self.change_x = 0
         self.change_y = 0
@@ -85,7 +100,7 @@ class Robot(pygame.sprite.Sprite):
         self.walls = None
         self.goals = None
         self.space = None
-  
+
     def update(self):
         """ Update the robot position. """
         # Move left/right=====
@@ -95,19 +110,19 @@ class Robot(pygame.sprite.Sprite):
             print ("Going West, Turning Left")
             self.face = (self.face - 1) % 4
             self.lhs = (self.lhs - 1) % 4
-            
+
 
         if((self.change_x>0) & (maze[int(self.rect.x/32)][int(self.rect.y/32)-1] != 1)):
             print ("Going East, Turning Left")
             self.face = (self.face - 1) % 4
             self.lhs = (self.lhs - 1) % 4
-            
+
 
         block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
         for block in block_hit_list:
-            
+
             # Wall to the east
-            if ((self.change_x > 0) & (self.face %4 == 1)): 
+            if ((self.change_x > 0) & (self.face %4 == 1)):
                 if((maze[int(self.rect.x/32)][int(self.rect.y/32)] == 0)):
                     print("what")
                 self.rect.x -= self.change_x
@@ -118,7 +133,7 @@ class Robot(pygame.sprite.Sprite):
                 self.lhs = (1 + self.lhs) % 4
 
             # Wall to the west
-            elif ((self.change_x < 0) & (self.face %4 ==3)): 
+            elif ((self.change_x < 0) & (self.face %4 ==3)):
                 self.rect.x -= self.change_x
                 print ("You've hit a wall at the West!")
                 self.rect.left = block.rect.right
@@ -133,20 +148,20 @@ class Robot(pygame.sprite.Sprite):
             print ("Going South, Turning Left")
             self.face = (self.face - 1) % 4
             self.lhs = (self.lhs - 1) % 4
-            
+
 
         if((self.change_y<0) & (maze[int(self.rect.x/32)-1][int(self.rect.y/32)] != 1)):
             print ("Going North, Turning Left")
             self.face = (self.face - 1) % 4
             self.lhs = (self.lhs - 1) % 4
-            
-             
- 
+
+
+
         block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
         for block in block_hit_list:
-    
+
             # Wall to the South
-            if ((self.change_y > 0) & (self.face %4 == 2)): 
+            if ((self.change_y > 0) & (self.face %4 == 2)):
                 self.rect.y -= self.change_y
                 print ("You've hit a wall at the South!")
                 self.rect.bottom = block.rect.top
@@ -167,21 +182,21 @@ class Robot(pygame.sprite.Sprite):
             print("You've solved the maze! Hooray!!!")
             pygame.quit()
             sys.exit(0)
-        
+
         self.change_x = 0
         self.change_y = 0
-        
-        
+
+
 class Wall(pygame.sprite.Sprite):
     """ Wall the robot can run into. """
     def __init__(self, x, y, width, height):
         """ Constructor for the wall that the robot can run into. """
         super().__init__()
- 
+
         # Make a black wall, of the size specified in the parameters
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
- 
+
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
         self.rect.y = y
@@ -192,58 +207,60 @@ class Space(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
         """ Constructor for the wall that the robot can run into. """
         super().__init__()
-  
+
         # Make a transparent wall, of the size specified in the parameters
         self.image = pygame.Surface([width, height], pygame.SRCALPHA, 32)
         self.image = self.image.convert_alpha()
-        
+
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
-        
+
 class Goal(pygame.sprite.Sprite):
     """ Wall the robot can run into. """
     def __init__(self, x, y, width, height):
         """ Constructor for the wall that the robot can run into. """
         # Call the parent's constructor
         super().__init__()
- 
+
         # Make the goal, of the size specified in the parameters
         self.image = pygame.Surface([width, height])
         self.image.fill(GOAL)
- 
+
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
-		 
-        
+
+
 # Left-Hand Rule wall following algorithm
 def LHRwallFollowing(robot, screen):
     face = robot.face # 0 is north, 1 is east, 2 is south, 3 is west
     LHS = robot.lhs
     position = robot
-    
-    #lhs is north
-    if(LHS%4 == 0):
-        print("Current Position: (", int((robot.rect.x/32)+2),", ", int((robot.rect.y/32)-1), ") --Going East")
-        robot.change_x += SPEED
-                        
-    #lhs is east
-    if(LHS%4 == 1):
-        print("Current Position: (", int((robot.rect.x/32)+2),", ", int((robot.rect.y/32)-1), ") --Going South")
-        robot.change_y += SPEED
 
-    #lhs is south
-    if(LHS%4 == 2):
-        print("Current Position: (",  int((robot.rect.x/32)+2),", ", int((robot.rect.y/32)-1), ") --Going West")
-        robot.change_x += -SPEED
+    robot.change_x = random.choice([32, -32])
+    robot.change_y = random.choice([32, -32])
+    # #lhs is north
+    # if(LHS%4 == 0):
+    #     print("Current Position: (", int((robot.rect.x/32)+2),", ", int((robot.rect.y/32)-1), ") --Going East")
+    #     robot.change_x += SPEED
 
-    #lhs is west
-    if(LHS%4 == 3):
-        print("Current Position: (",  int((robot.rect.x/32)+2), ", ", int((robot.rect.y/32)-1), ") --Going North")
-        robot.change_y += -SPEED
+    # #lhs is east
+    # if(LHS%4 == 1):
+    #     print("Current Position: (", int((robot.rect.x/32)+2),", ", int((robot.rect.y/32)-1), ") --Going South")
+    #     robot.change_y += SPEED
+
+    # #lhs is south
+    # if(LHS%4 == 2):
+    #     print("Current Position: (",  int((robot.rect.x/32)+2),", ", int((robot.rect.y/32)-1), ") --Going West")
+    #     robot.change_x += -SPEED
+
+    # #lhs is west
+    # if(LHS%4 == 3):
+    #     print("Current Position: (",  int((robot.rect.x/32)+2), ", ", int((robot.rect.y/32)-1), ") --Going North")
+    #     robot.change_y += -SPEED
 
 
 def main():
@@ -255,16 +272,19 @@ def main():
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill(WHITE)
-    
+
     # Make the walls. (x_pos, y_pos, width, height)
     wall_list = pygame.sprite.Group()
-    
+
     # Make the goal. (x_pos, y_pos, width, height)
     goal_list = pygame.sprite.Group()
-    
+
     # Make the space. (x_pos, y_pos, width, height)
     space_list = pygame.sprite.Group()
-    
+
+    # robots
+    robots = pygame.sprite.Group()
+
     for j in range(0, n):
         for i in range(0, m):
             if(maze[i][j] == 1):
@@ -280,39 +300,42 @@ def main():
                 space_list.add(space)
                 all_sprite_list.add(space)
 
-    robot = Robot(-64, 32)
-    robot.face = 1
-    robot.lhs = 0
-    robot.walls = wall_list
-    robot.goals = goal_list
-    robot.space = space_list
+    for k, color in COLORS.items():
+        robot = Robot(-64, 32, color=color)
+        robot.face = 1
+        robot.lhs = 0
+        robot.walls = wall_list
+        robot.goals = goal_list
+        robot.space = space_list
 
-    all_sprite_list.add(robot)
+        robots.add(robot)
+        all_sprite_list.add(robot)
 
     clock = pygame.time.Clock()
 
     done = False
-    
+
     while not done:
-    
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
 
-        LHRwallFollowing(robot, screen)
-    
+        for robot in robots:
+            LHRwallFollowing(robot, screen)
+
         all_sprite_list.update()
- 
+
         screen.fill(GREY)
-    
+
         all_sprite_list.draw(screen)
-    
+
         pygame.display.flip()
-    
+
         clock.tick(60)
         pygame.time.wait(timer)
-        
+
     pygame.quit()
-    
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     main()
